cfc36612c9ae01db0c330828616201c9d1e3d2d4
savaofn <iaroslav_frolov@mail.ru>
2023-02-16 12:48:28 +0200

    First

---
+++ date-time/src/time/WorkingDays.java
@@ -0,0 +1,18 @@
+package time;
+
+import java.time.DayOfWeek;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class WorkingDays implements TemporalAdjuster {
+//TODO
+	@Override
+	public Temporal adjustInto(Temporal temporal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public WorkingDays(DayOfWeek[] dayOffs) {
+		//TODO
+	}
+
+}
